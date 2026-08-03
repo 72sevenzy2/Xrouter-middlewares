480e05db27f683cdfde4df175078d3d76bf57915
72 <72sevenzy2@gmail.com>
2026-08-03 18:04:46 +0500

    feat: add initial rate limiter (capped at 100 requests).

--- rate_limiter.go
+++ rate_limiter.go
@@ -1,3 +1,52 @@
 package middleware
 
+import (
+	"net/http"
+	"sync"
+	"time"
 
+	"github.com/72sevenzy2/http-router/core"
+	"github.com/72sevenzy2/json-parser/response"
+)
+
+// client request details
+type client struct {
+	requests int
+	resetTime time.Time
+}
+
+var clients = map[string]*client{}
+var mu sync.Mutex // cleanup gorountine will also modify client.
+
+func RateLimiter() core.Middleware {
+	return func(hf core.HandlerFunc) core.HandlerFunc {
+		return func(w http.ResponseWriter, r *core.Request) {
+			clientIp := r.RemoteAddr
+
+			mu.Lock()
+			c, ok := clients[clientIp]
+			if !ok {
+				c = &client{
+					resetTime: time.Now().Add(time.Minute),
+				}
+
+				clients[clientIp] = c
+			}
+
+			if time.Now().After(c.resetTime) {
+				c.requests = 0
+				c.resetTime = time.Now().Add(time.Minute)
+			}
+
+			c.requests++
+
+
+			if c.requests > 100 { // 100 requests cap for now
+				mu.Unlock()
+				response.JSON(w, response.WithError(http.StatusText(http.StatusTooManyRequests)), response.WithStatus(http.StatusTooManyRequests))
+				return
+			}
+
+		}
+	}
+}
