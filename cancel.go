5e4e65640e751f4e0d6493c4d90b4340df1891d1
72 <72sevenzy2@gmail.com>
2026-08-30 19:54:18 +0500

    feat: add small context cancellation mw.

---
+++ cancel.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/72sevenzy2/http-router/core"
+)
+
+func Canceller() (core.Middleware, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	mw := func(hf core.HandlerFunc) core.HandlerFunc {
+		return func(w http.ResponseWriter, r *core.Request) {
+			reqCtx, c := context.WithCancel(ctx)
+			defer c()
+
+			r.ContextReq = r.ContextReq.WithContext(reqCtx)
+			hf(w, r)
+		}
+	}
+
+	return mw, cancel
+}
